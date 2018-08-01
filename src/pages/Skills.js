a37d7b20b8c4a952abb47425cfb6dc19ec691efe
David Rausch <David@Davids-MacBook-Pro-2.local>
2018-08-01 11:52:17 -0600

    skills additions

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -19,7 +19,8 @@ class Skills extends Component{
                 <Row>
                     <Col l = {12}>
                     <h2 className = "dev-skills">Development Skills</h2>
-                    <p className = "dev-p">NOTE: This chart is merely a guideline of my skills as I am constantly updating my knowledge daily</p>
+                    <p className = "dev-p">NOTE: This chart is merely a guideline of my skills as I am constantly updating my knowledge daily and there are more technologies
+                    that could be added</p>
                     </Col>
                 </Row>
                 <Row>
@@ -65,10 +66,31 @@ class Skills extends Component{
                             </ul>
                     </Col>
                 </Row>
-                <Row></Row>
+                <Row>
+                    <Col l = {12}>
+                        <h3 className = "education">Formal Developer Education</h3>
+                    </Col>
+                </Row>
+                
+                <Row>
+                    <Col l = {12}>
+                        <h5 className = "boot-camp"data-aos = "fade-right" data-aos-duration="1800">University of Denver Coding Boot Camp(April 2018-July 2018)</h5>
+                        <br />
+                        <p className = "bc-info"data-aos = "fade-right" data-aos-duration="1800">Attended 3 month coding boot camp where I received formal education on the
+                        following technologies: HTML, CSS, Javascript, jQuery, ReactJS, NodeJS, Bootstrap, MySQL, Sequelize, MongoDB, Mongoose, HandlebarsJS, API integration,
+                        Git, Firebase, ExpressJS, PHP, and Redux.</p>
+                    </Col>
+                </Row>
                 <br />
-                <Row></Row>
-                <Row></Row>
+                <br />
+                <Row>
+                    <Col l = {12}>
+                        <h5 className = "regis"data-aos = "fade-right" data-aos-duration="1800">Regis University(January 2016-Tentative Graduation December 2019)</h5>
+                        <br />
+                        <p className = "regis-info"data-aos = "fade-right" data-aos-duration="1800">Pursuing a BS degree in Computer Science.  Curriculum has included 
+                        programming in Java, Discrete Mathematics/Logic, Computational Foundations, Artificial Intelligence, Data Structures, Computation Theory, and Algorithms and Complexity.</p> 
+                    </Col>
+                </Row>
                 <br />
                 <Row></Row>
                 <Row></Row>
